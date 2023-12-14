976a47230e6b045aa36ba5d3edb621a858013cb8
Sergiy Leshchak <118683335+UFO73@users.noreply.github.com>
2023-12-14 18:56:49 +0200

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import styles from '../index.css'
+import { useRef } from 'react';
+import Header from './Header/header';
+import StickyCursor from './StickyCursor/stickyCursor';
+
 export const App = () => {
+  const stickyElement = useRef();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <main className={styles.main}>
+      <Header ref={stickyElement}/>
+      <StickyCursor stickyElement={stickyElement}/>
+    </main>
   );
 };
